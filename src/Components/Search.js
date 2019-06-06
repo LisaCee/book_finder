5d86b9cd2284d75f30456e65f32d3a9b4bee65a9
LisaCee <lisaceedesign@gmail.com>
2019-06-05 21:26:54 -0700

    refactor parent/child

--- src/Components/Search.js
+++ src/Components/Search.js
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
+import BookCard from "./BookCard";
 
 class Search extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      query: ""
+      searchTerm: "",
+      items: []
     };
   }
   handleInput = e => {
@@ -12,21 +14,44 @@ class Search extends Component {
     this.setState({ [e.target.name]: e.target.value });
   };
   onSearch = () => {
-    const queryString = this.state.query;
+    let searchTerm = this.state.searchTerm;
+    let baseURL = `https://www.googleapis.com/books/v1/volumes?q=${searchTerm}`;
+    fetch(baseURL)
+      .then(response => {
+        return response.json();
+      })
+      .then(data => {
+        console.log(data);
+        this.setState({ items: data.items });
+        this.setState({ searchTerm: "" });
+      })
+      .catch(error => {
+        console.log("Error", error);
+      });
+  };
+  onEnterButton = e => {
+    if (e.key === "Enter") {
+      this.onSearch();
+    }
   };
   render() {
     return (
-      <div>
+      <div className="searchScreen">
+        <h1>Blind Date with a Book</h1>
         <input
           type="text"
-          name="query"
-          value={this.state.query}
+          name="searchTerm"
+          value={this.state.searchTerm}
           onChange={this.handleInput}
           placeholder="Author, Title, or Keyword"
+          onKeyPress={this.onEnterButton}
         />
         <button type="submit" onClick={this.onSearch}>
           Go
         </button>
+        <div>
+          <BookCard book={this.state.items} />
+        </div>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,18 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Books from "./Components/Books";
 import Search from "./Components/Search";
-import Book from "./Components/Book";
+import "./styles.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import "./styles.css"
 
 function App() {
   return (
     <div className="App">
-      <h1>Don't judge a book by its cover</h1>
-      {/* <Book /> */}
       <Search />
-      <Books />
     </div>
   );
 }
